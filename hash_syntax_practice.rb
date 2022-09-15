f386ed35d24c2f1f2f9be6a8d47947d569d6e3d5
Thomas Casady <tscasady@gmail.com>
2022-09-15 09:56:03 -0500

    Make hash more readable

--- hash_syntax_practice.rb
+++ hash_syntax_practice.rb
@@ -1,4 +1,8 @@
-store_inventory = {'apples' => 16, 'oranges' => 22, 'mango' => 7}
+store_inventory = {
+  'apples' => 16,
+  'oranges' => 22,
+  'mango' => 7
+}
 
 tweet = {'text' => 'My M1 Macbook Pro arrived', 'Twitter Handle' => '@MechEngSanchesz'}
 
